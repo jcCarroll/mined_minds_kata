3e22df440b0ae43559a0f3891e3dcb43b6e5cce5
jacob <2staffi@gmail.com>
2016-08-23 13:34:44 -0400

    notes showing work

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,3 +1,31 @@
+# print numbers 1 to 100
+# if number is exactly divisible by 3 replace with “Mined”
+# if number is exactly divisible by 5 replace with “Minds”
+# if number is exactly divisible by 3 and 5 replace with “Mined Minds"
+
+# divis by 3
+# x = 1
+# 100.times do
+# 	if x % 3 == 0
+# 		puts "mined"
+# 	else puts x
+# 	end
+# 	x = x + 1
+# end
+
+# added divis by 5
+# x = 1
+# 100.times do
+# 	if x % 3 == 0
+# 		puts "mined"
+# 	elsif x % 5 == 0
+# 		puts "minds"
+# 	else puts x
+# 	end
+# 	x = x + 1
+# end
+
+# added divis by 3 and 5
 x = 1
 100.times do
 	if (x % 3 == 0 and x % 5 == 0)
