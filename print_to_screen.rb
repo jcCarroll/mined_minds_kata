ccddd8f70c559df632ea02783d57a74d1d8b12e0
jacob <2staffi@gmail.com>
2016-08-23 13:10:15 -0400

    moved x = x + 1 to bottom of equation

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,6 +1,5 @@
-x = 0
+x = 1
 100.times do
-	x = x + 1
 	if (x % 3 == 0 and x % 5 == 0)
 		puts "mined minds"
 	elsif (x % 3 == 0)
@@ -9,4 +8,5 @@ x = 0
 		puts "minds"
 	else puts x
 	end
+	x = x + 1
 end
