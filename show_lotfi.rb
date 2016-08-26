d8507a6b3ff0fda977c81be9b50105ef76f89406
jacob <2staffi@gmail.com>
2016-08-25 20:25:38 -0400

    code for return pop

--- show_lotfi.rb
+++ show_lotfi.rb
@@ -2,13 +2,18 @@ def show_lotfi(number)
 	if number == 3
 		"lotfi"
 	else 
-	number - 1
+		number - 1
 	end
 end
 
 def what(number)
 	if number % 3 == 0 && number % 5 == 0
 		"soda"
+	elsif 
+		number % 2 == 0 && number % 3 == 0
+		"pop"
+	else
+		number		
 	end	
 end
 
@@ -16,6 +21,6 @@ def show_jake(number)
 	if number % 3 == 0
 		"monkey"
 	else
-	number
+		number
 	end
 end
