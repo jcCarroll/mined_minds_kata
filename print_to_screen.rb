c9a3be33b1bae026d068fd22561753e6b96140a3
jacob <2staffi@gmail.com>
2016-08-25 21:05:38 -0400

    cleaned up the code

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -28,13 +28,16 @@
 # added divis by 3 and 5
 number = 1
 100.times do
-	if (number % 3 == 0 && number % 5 == 0)
-		puts "mined minds"
-	elsif (number % 3 == 0)
-		puts "mined"
-	elsif (number % 5 == 0)
-		puts "minds"
-	else puts number
+	if number % 3 == 0 && number % 5 == 0
+		"mined minds"
+	elsif 
+		number % 3 == 0
+		"mined"
+	elsif 
+		number % 5 == 0
+		"minds"
+	else
+		number
 	end
 	number = number + 1
 end
