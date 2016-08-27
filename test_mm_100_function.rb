8fe717c4d12b006641f162263a5f193eacea674b
jacob <2staffi@gmail.com>
2016-08-27 00:38:03 -0400

    test 3 return mined

--- test_mm_100_function.rb
+++ test_mm_100_function.rb
@@ -6,7 +6,11 @@ class TestMM100Function <Minitest::Test
 		assert_equal(1, jacob(1))
 	end
 
-	def test_5447_returns_jacob_christopher_carroll
+	def test_5_returns_jacob_christopher_carroll
 		assert_includes(["jacob", "christopher", "carroll"], jacob(5))
+	end
+
+	def test_3_returns_mined
+		assert_equal(mined, jacob(3))
 	end
 end
