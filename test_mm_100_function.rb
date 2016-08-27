8daff2e81e2a968688a349a558d85555477c01a0
jacob <2staffi@gmail.com>
2016-08-26 22:46:45 -0400

    passing number for random return

--- test_mm_100_function.rb
+++ test_mm_100_function.rb
@@ -3,6 +3,10 @@ require_relative "mm_100_function.rb"
 
 class TestMM100Function <Minitest::Test 
 	def test_1_equals_1
-		assert_equal(1, 1)
+		assert_equal(1, jacob(1))
+	end
+
+	def test_5447_returns_jacob_christopher_carroll_random
+		assert_equal([jacob,christopher.carroll].random, jacob(5447))
 	end
 end
