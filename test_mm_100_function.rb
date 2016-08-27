56a58e8720806aa6d64f9459a96a1689d0557fad
jacob <2staffi@gmail.com>
2016-08-27 01:16:11 -0400

    added jacob(1) to test 1

--- test_mm_100_function.rb
+++ test_mm_100_function.rb
@@ -3,7 +3,7 @@ require_relative "mm_100_function.rb"
 
 class TestMM100Function <Minitest::Test 
 	def test_1_equals_1
-		assert_equal(1, 1)
+		assert_equal(1, jacob(1))
 	end
 
 	def test_5_returns_jacob_christopher_carroll
