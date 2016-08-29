86682f16fc0f834d85c064a615efd4516776b9e9
jacob <2staffi@gmail.com>
2016-08-28 22:23:52 -0400

    removed puts, tested working

--- mined_minds_function.rb
+++ mined_minds_function.rb
@@ -1,12 +1,12 @@
 def mined_minds(number)
-	if number % 3 == 0 && number % 5 ==0  # test something that has a remainder
-		puts "mined minds"
+	if number % 3 == 0 && number % 5 ==0
+		"mined minds"
 	elsif 
 		number % 3 == 0
-		puts "mined"
+		"mined"
 	elsif
 		number % 5 ==0
-		puts "minds"		
+		"minds"
 	else 
 		number
 	end
