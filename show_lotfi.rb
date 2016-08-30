68632e8a61e962ba115a8f66bd250e8c89d0c705
jacob <2staffi@gmail.com>
2016-08-30 09:22:52 -0400

    Testing

--- show_lotfi.rb
+++ show_lotfi.rb
@@ -1,30 +1,59 @@
-number = 1
-30.times do
+# number = 1
+# 30.times do
+# 	def show_lotfi(number)
+# 		if number == 3
+# 			puts "lotfi"
+# 		else 
+# 			puts number
+# 		end
+# 	end
+
+# 	def what(number)
+# 		if number % 3 == 0 && number % 5 == 0
+# 			puts "soda"
+# 		elsif 
+# 			number % 2 == 0 && number % 3 == 0
+# 			puts "pop"
+# 		else
+# 			puts number		
+# 		end	
+# 	end
+
+# 	def show_jake(number)
+# 		if number % 3 == 0
+# 			puts "monkey"
+# 		else
+# 			puts number
+# 		end
+# 	end
+# 	number = number + 1
+# end
+
+
 	def show_lotfi(number)
 		if number == 3
-			puts "lotfi"
+			"lotfi"
 		else 
-			puts number
+			number
 		end
 	end
 
 	def what(number)
 		if number % 3 == 0 && number % 5 == 0
-			puts "soda"
+			"soda"
 		elsif 
 			number % 2 == 0 && number % 3 == 0
-			puts "pop"
+			"pop"
 		else
-			puts number		
+			number		
 		end	
 	end
 
 	def show_jake(number)
 		if number % 3 == 0
-			puts "monkey"
+			"monkey"
 		else
-			puts number
+			number
 		end
 	end
-	number = number + 1
-end
+	
