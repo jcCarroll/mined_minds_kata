0c7e006b1223ebaaaf1e00da0942306ef7e2a460
jacob <2staffi@gmail.com>
2016-08-30 11:48:33 -0400

    more testing

--- test_addition.rb
+++ test_addition.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require_relative "addition.rb"
 require_relative "subtraction.rb"
+require_relative "division.rb"
 
 class TestAdditionFunction <Minitest::Test 
 	def test_1_equals_1
@@ -32,7 +33,28 @@ class TestSubtractionFunction <Minitest::Test
 	def test_10_subtract_5
 		assert_equal(5, sub(10, 5))
 	end
+end
 
+class TestDivideFunction <Minitest::Test
+	def test_6_divide_3
+		assert_equal(2, div(6, 3))
+	end
 
+	def test_100_divide_0
+		assert_equal(100, div(100, 0))
+	end
+end
 
+class TestAllFunction <Minitest::Test
+	def test_1_plus_2
+		assert_equal(3, add(1,2))
+	end
+
+	def test_1_subtract_2
+		assert_equal(-1, sub(1,2))
+	end
+
+	def test_1_divide_2
+		assert_equal(0.5, div(1.0, 2))
+	end
 end
