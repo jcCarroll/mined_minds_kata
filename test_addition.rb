571149dda7a956a6bdaef550293fb90796542b14
jacob <2staffi@gmail.com>
2016-08-30 11:19:51 -0400

    test 1 sub 1

--- test_addition.rb
+++ test_addition.rb
@@ -1,5 +1,6 @@
 require "minitest/autorun"
 require_relative "addition.rb"
+require_relative "subtraction.rb"
 
 class TestAdditionFunction <Minitest::Test 
 	def test_1_equals_1
@@ -21,7 +22,12 @@ class TestAdditionFunction <Minitest::Test
 	def test_5_plus_neg3
 		assert_equal(2, add(5, -3))
 	end
+end
 
+class TestSubtractionFunction <Minitest::Test
+	def test_1_subtract_1
+		assert_equal(0, sub(1, 1))
+	end
 
 
 
