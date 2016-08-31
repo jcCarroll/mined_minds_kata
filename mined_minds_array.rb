5d4694286f2f3aed1ad880c6942e002ecc047cee
jacob <2staffi@gmail.com>
2016-08-30 21:56:29 -0400

    thought function could be in array

--- mined_minds_array.rb
+++ mined_minds_array.rb
@@ -1,21 +1,19 @@
 def mined_minds
-	number = 0
-100.times do
-	if number % 3 == 0 && number % 5 ==0
-		"mined minds"
-	elsif 
-		number % 3 == 0
-		"mined"
-	elsif
-		number % 5 ==0
-		"minds"
-	else 
-		number
+	number = 1
+	100.times do
+		if number % 3 == 0 && number % 5 ==0
+			"mined minds"
+		elsif 
+			number % 3 == 0
+			"mined"
+		elsif
+			number % 5 ==0
+			"minds"
+		else 
+			number
+		end
+		number = number + 1
 	end
-	
-	number = number + 1
 end
-    mined_minds_array = []
-	mined_minds_array.collect {|item| block }
 
-end
+array = [mined_minds]
