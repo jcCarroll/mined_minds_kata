ff9fb7420bcb7ed9e4c0a6d2aca5f59b096434a8
jacob <2staffi@gmail.com>
2016-08-30 21:55:21 -0400

    it didn't like .first, replaced with [0]

--- test_mined_minds_array.rb
+++ test_mined_minds_array.rb
@@ -4,7 +4,7 @@ require_relative "mined_minds_array.rb"
 class TestMinedMindsFunction <Minitest::Test 
 def test_first_element_1
 		result = mined_minds()
-		assert_equal(1, result.first)
+		assert_equal(1, result[0])
 	end
 end
 
