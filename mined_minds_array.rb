c73413d72e44d452d7623785d3dbc4ec0347d764
jacob <2staffi@gmail.com>
2016-09-02 16:52:46 -0400

    added code to print to screen

--- mined_minds_array.rb
+++ mined_minds_array.rb
@@ -13,4 +13,21 @@ def mined()
       		 element
    		end
    	}
-   	end
+end
+
+
+# array = [*1..100]
+# 	array.map!  {|element|
+# 		if element % 3 == 0 && element % 5 == 0
+# 			"minedminds"
+#    		elsif
+#        		element % 3 == 0
+#        		"mined"
+#    		elsif
+#       		element % 5 == 0
+#        		"minds"
+#    		else
+#       		 element
+#    		end
+#    	}
+#    		puts array
