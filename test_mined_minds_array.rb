7af7ad8bab4e10d4e36902e3831b14b9076e427d
jacob <2staffi@gmail.com>
2016-09-02 16:41:26 -0400

    added tests minds and minedminds

--- test_mined_minds_array.rb
+++ test_mined_minds_array.rb
@@ -2,9 +2,22 @@ require "minitest/autorun"
 require_relative "mined_minds_array.rb"
 
 class TestMinedMindsFunction <Minitest::Test 
-def test_first_element_1
-		# result = mined()
+    def test_first_element_1
+		result = mined()
 		assert_equal(1, mined[0])
+	end
+
+	def test_element_2_mined
+		result = mined()
+		assert_equal("mined", mined[2])
+	end
+	def test_element_4_minds
+		result = mined()
+		assert_equal("minds", mined[4])
+	end
+	def test_element_14_minedminds
+		result = mined()
+		assert_equal("minedminds", mined[14])
 	end
 end
 
