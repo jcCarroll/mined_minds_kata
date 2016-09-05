3833898a850f1cbd0db5d3a34fa6d60e66f119a3
jacob <2staffi@gmail.com>
2016-09-05 12:20:36 -0400

    got array to pass to test

--- Wk3/mined_minds_array.rb
+++ Wk3/mined_minds_array.rb
@@ -1,28 +1,31 @@
 # i don't think this is the right way. it seems that i should be taking the
 # array to the test and then going through it.
 def mined()
-	array = [*1..100]
+	mm_array = [*1..100]
 	# building an array numbering from 1 to 100
-	array.map do |element|
+	mm_array.map! do |element|
 	# not 100% on what map does. i think it takes each element in the array
 	# passes it through the block (if statements) and replaces the element in 
 	# the array with what the block returns
 	# the || tells my function that each element is now set to a variable (and 
 	# the what the variable is |element|) that it will cycle through my array
 	# and then to run each pass through the if statements
-	if element % 15 == 0
-		"minedminds"
- 	elsif
-     	element % 3 == 0
-     	"mined"
- 	elsif
-     	element % 5 == 0
-     	"minds"
- 	else
-     	element
- 	end
- end
+		if element % 15 == 0
+			"minedminds"
+ 		elsif
+     		element % 3 == 0
+     		"mined"
+ 		elsif
+ 	    	element % 5 == 0
+ 	    	"minds"
+ 		else
+  		   	element
+ 		end
+	end
+	mm_array
 end
+
+
 
 ###### Below - Short code ######
 
--- Wk3/test_mined_minds_array.rb
+++ Wk3/test_mined_minds_array.rb
@@ -6,33 +6,33 @@ require_relative "mined_minds_array.rb"
 class TestMinedMindsFunction <Minitest::Test
 	
     def test_element_0
-		# result = mined()
-		assert_equal(1, mined[0])
+		result = mined()
+		assert_equal(2, result[1])
 	end
 
 	def test_element_2_mined
-		# result = mined()
-		assert_equal("mined", mined[2])
+		result = mined()
+		assert_equal("mined", result[2])
 	end
 
 	def test_element_4_minds
-		# result = mined()
-		assert_equal("minds", mined[4])
+		result = mined()
+		assert_equal("minds", result[4])
 	end
 
 	def test_element_14_minedminds
 		result = mined()
-		assert_equal("minedminds", mined[14])
+		assert_equal("minedminds", result[14])
 	end
 
 	def test_first_element
 		result = mined()
-		assert_equal(1, mined.first)
+		assert_equal(1, result.first)
 	end
 
 	def test_last_element
 		result = mined()
-		assert_equal("minds", mined.last)
+		assert_equal("minds", result.last)
 	end
 end
 
