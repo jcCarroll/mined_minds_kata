65e8c32a5efef699ad1ac753a2924c7d68e6549b
2staffi@gmail.com <2staffi@gmail.com>
2017-10-06 21:21:54 -0400

    20171006

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,25 @@
-# i know how everything works but this top line
+#10/6/2017
 1.upto(100) do |i|
 	if i % 5 == 0 and i % 3 == 0
 		puts "FizzBuzz"
 	elsif i % 5 == 0
 		puts "Buzz"
 	elsif i % 3 == 0
-		puts "Fizz"
+		puts "fizz"
 	else
 		puts i
 	end
 end
+
+# i know how everything works but this top line
+# 1.upto(100) do |i|
+# 	if i % 5 == 0 and i % 3 == 0
+# 		puts "FizzBuzz"
+# 	elsif i % 5 == 0
+# 		puts "Buzz"
+# 	elsif i % 3 == 0
+# 		puts "Fizz"
+# 	else
+# 		puts i
+# 	end
+# end
